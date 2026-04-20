8fe3aa54b0a641c6cd23f8f7d2754d2b24324873
cerdocyonina <jsjsjsksjkakak028@gmail.com>
2026-04-20 22:00:58 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/cerdocyonina/sr-rules-gen
+
+go 1.26.2
+
+require (
+	github.com/golang/protobuf v1.5.4
+	github.com/v2fly/v2ray-core/v5 v5.49.0
+)
+
+require (
+	github.com/adrg/xdg v0.5.3 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/v2fly/v2ray-core/v5/app/router/routercommon"
+	"google.golang.org/protobuf/proto"
+)
+
+func geositeRuleToSrRule(rule *routercommon.Domain) string {
+	var srStrategy string
+
+	// TODO: check all enum values are covered?
+	switch rule.Type {
+	case routercommon.Domain_Plain:
+		srStrategy = "DOMAIN-KEYWORD"
+	case routercommon.Domain_Full:
+		srStrategy = "DOMAIN"
+	case routercommon.Domain_RootDomain:
+		srStrategy = "DOMAIN-SUFFIX"
+	case routercommon.Domain_Regex:
+		srStrategy = "URL-REGEX"
+	default:
+		srStrategy = "DOMAIN"
+	}
+	return srStrategy + "," + rule.Value
+}
+
+func processCategory(category *routercommon.GeoSite) {
+	fmt.Printf("processing category %v...\n", category.CountryCode)
+	// fmt.Printf("entry: %v\n", category.CountryCode)
+	t0 := time.Now()
+
+	fileName := fmt.Sprintf("%v.list", strings.ToLower(category.CountryCode))
+	file, err := os.Create(path.Join("output", fileName))
+	if err != nil {
+		fmt.Printf("failed create file for category %v: %v", category.CountryCode, err)
+		return
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	for _, rule := range category.Domain {
+		_, err := writer.Write([]byte(geositeRuleToSrRule(rule) + "\n"))
+		if err != nil {
+			fmt.Printf("write err: %v\n", err)
+			return
+		}
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Printf("flush err: %v\n", err)
+	}
+
+	fmt.Printf("processed category %v in %v\n", category.CountryCode, time.Since(t0))
+}
+
+func main() {
+	// resp, err := http.Get("https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/latest/download/geosite.dat")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+
+	// fmt.Println("downloading geosite...")
+	// bodyBytes, err := io.ReadAll(resp.Body)
+	// fmt.Printf("read %d bytes (%.2fM)\n", len(bodyBytes), float64(len(bodyBytes))/1024/1024
+
+	bodyBytes, err := os.ReadFile("temp/geosite.dat")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("parsing geosite...")
+	geositeList := &routercommon.GeoSiteList{}
+	err = proto.Unmarshal(bodyBytes, geositeList)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("parsed geosite")
+
+	err = os.Mkdir("output", 0700)
+	wg := sync.WaitGroup{}
+	t0 := time.Now()
+	for _, entry := range geositeList.GetEntry() {
+		wg.Add(1)
+		go func(s *routercommon.GeoSite) {
+			defer wg.Done()
+			processCategory(s)
+		}(entry)
+	}
+	wg.Wait()
+	fmt.Printf("finished in %vms\n", time.Since(t0).Milliseconds())
+}
